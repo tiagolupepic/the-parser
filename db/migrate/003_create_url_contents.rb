b22a43727471627a064efe606f99b1c19c63fbd8
Tiago Lupepic <tiagolpc@gmail.com>
2016-08-16 20:24:19 -0300

    Change text to hstore to headers and links

--- db/migrate/003_create_url_contents.rb
+++ db/migrate/003_create_url_contents.rb
@@ -1,8 +1,11 @@
 class CreateUrlContents < ActiveRecord::Migration[5.0]
   def change
     create_table :url_contents, id: :uuid do |t|
-      t.text :name, :header_one, :header_two, :header_three
-      t.hstore :links
+      t.text :name
+      t.hstore :headers_one  , array: true
+      t.hstore :headers_two  , array: true
+      t.hstore :headers_three, array: true
+      t.hstore :links        , array: true
 
       t.timestamps null: false
     end
