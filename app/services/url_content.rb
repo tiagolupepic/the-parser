15f205415928f7a56dda0c6063b937606ca851f8
Tiago Lupepic <tiagolpc@gmail.com>
2016-08-16 20:29:13 -0300

    Add ability to UrlContentService to not parse when url is blank

--- app/services/url_content.rb
+++ app/services/url_content.rb
@@ -1,2 +1,11 @@
 class UrlContentService
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def run
+    return if name.blank?
+  end
 end
--- spec/services/url_content_spec.rb
+++ spec/services/url_content_spec.rb
@@ -1,4 +1,30 @@
 require 'spec_helper'
 
 RSpec.describe UrlContentService do
+  subject { described_class.new(url) }
+
+  describe '#run' do
+    context 'with blank url' do
+      let(:url) { nil }
+
+      it 'should not create UrlContent' do
+        subject.run
+
+        expect(UrlContent.count).to eq 0
+      end
+
+      it 'should not raise error' do
+        expect { subject.run }.to_not raise_error
+      end
+    end
+
+    context 'with valid url' do
+    end
+
+    context 'with invalid url' do
+    end
+
+    context 'with url shortener' do
+    end
+  end
 end
