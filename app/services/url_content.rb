d5b28be8befd5cf338cc0b4ad0aebe06a1d8b3fc
Tiago Lupepic <tiagolpc@gmail.com>
2016-08-16 21:24:13 -0300

    Add ability to UrlContentService parse a valid url and assign attributes

--- app/services/url_content.rb
+++ app/services/url_content.rb
@@ -1,11 +1,40 @@
 class UrlContentService
-  attr_reader :name
+  attr_reader :url
 
-  def initialize(name)
-    @name = name
+  def initialize(url)
+    @url = url
   end
 
   def run
-    return if name.blank?
+    return if url.blank?
+    create_url_with_content
+  end
+
+  private
+
+  def create_url_with_content
+    UrlContent.create! params
+  end
+
+  def document
+    @document ||= open(url)
+  end
+
+  def page
+    @page ||= Nokogiri::HTML(document)
+  end
+
+  def params
+    {
+      name:          url,
+      headers_one:   page.css('h1').map(&:text),
+      headers_two:   page.css('h2').map(&:text),
+      headers_three: page.css('h3').map(&:text),
+      links:         page_links
+    }
+  end
+
+  def page_links
+    page.css('a').collect { |link| link.attributes['href'].value }
   end
 end
--- spec/services/url_content_spec.rb
+++ spec/services/url_content_spec.rb
@@ -19,6 +19,34 @@ RSpec.describe UrlContentService do
     end
 
     context 'with valid url' do
+      let(:url) { 'http://guides.rubyonrails.org/routing.html' }
+
+      it 'should create UrlContent' do
+        subject.run
+
+        expect(UrlContent.count).to eq 1
+      end
+
+      it 'should assign params' do
+        subject.run
+
+        url = UrlContent.first
+
+        expect(url.name).to               eq 'http://guides.rubyonrails.org/routing.html'
+        expect(url.headers_one).to        eq ['Guides.rubyonrails.org']
+        expect(url.headers_two).to        eq ['Rails Routing from the Outside In']
+        expect(url.headers_three.size).to eq 7
+        expect(url.links.size).to         eq 112
+      end
+
+      it 'should assign links urls' do
+        subject.run
+
+        url = UrlContent.first
+
+        expect(url.links.first).to eq 'http://rubyonrails.org/'
+        expect(url.links.last).to  eq 'https://creativecommons.org/licenses/by-sa/4.0/'
+      end
     end
 
     context 'with invalid url' do
